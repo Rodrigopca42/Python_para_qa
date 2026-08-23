d009ea4aa91d38d927a8469a24bcbf43d6ac461a
Rodrigopca42 <rodrigopca426281@gmail.com>
2026-08-23 17:45:31 -0300

    estudo Comentários e boas práticas iniciais

---
+++ Comentários_e_boas_práticas_iniciais.py
@@ -0,0 +1,20 @@
+
+'''1. Espaçamento ajuda a leitura'''
+#print("Resultado:",10+5)
+#print("Resultado:", 10 + 5)
+
+'''2. Quebre o código em partes claras'''
+'''Não faça'''
+#a=10;b=5;
+#print(a+b)
+
+'''Correto'''
+a = 10
+b = 5
+soma = a + b
+print(soma)
+
+
+
+
+
