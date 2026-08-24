d1829e1ae844607d7757af8422d1cc19cf4ebf3a
Rodrigopca42 <rodrigopca426281@gmail.com>
2026-08-24 18:47:44 -0300

    Conversão de tipos (casting)

---
+++ Conversão_de_tipos_(casting).py
@@ -0,0 +1,29 @@
+'''valor1 = input("Digite um número: ")
+valor2 = input("Digite outro número: ")
+print(valor1 + valor2)'''
+
+
+'''numero_texto = input("Digite um número inteiro: ")
+numero = int(numero_texto)
+print(numero)
+print(type(numero))'''
+
+
+'''valor_texto = input("Digite um valor decimal: ")
+valor = float(valor_texto)
+print(valor)
+print(type(valor))'''
+
+
+'''total = 150
+mensagem = "Valor total: " + str(total)
+print(mensagem)'''
+
+'''valor1 = int(input("Digite o primeiro número: "))
+valor2 = int(input("Digite o segundo número: "))
+resultado = valor1 + valor2
+print(f"Resultado da soma: {resultado}")'''
+
+
+
+
