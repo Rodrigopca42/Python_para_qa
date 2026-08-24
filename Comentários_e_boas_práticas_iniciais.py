48f345353b39a2d6507ff5757db5ab79e19452ef
Rodrigopca42 <rodrigopca426281@gmail.com>
2026-08-24 18:16:44 -0300

    Operadores lógicos, aritméticos e relacionais

--- Comentários_e_boas_práticas_iniciais.py
+++ Comentários_e_boas_práticas_iniciais.py
@@ -14,6 +14,40 @@ b = 5
 soma = a + b
 print(soma)
 
+'''#3. Organização visual importe'''
+'''Use linhas em branco para separar blocos lógicos:'''
+
+valor =10
+
+resultado = valor * 2
+
+print(resultado)
+
+'''4. Código deve explicar a si mesmo'''
+#Se você precisa explicar demais com comentários, o código pode melhorar.
+#Exemplo ruim:
+
+# Soma a com b
+# Depois imprime
+# Resultado final
+print(a + b)
+
+#Exemplo melhor:
+resultado = a + b
+print(resultado)
+
+'''Ligação com QA
+Para QA, essas práticas são críticas porque:
+Testes precisam ser claros
+Scripts são mantidos por times
+Debug exige leitura rápida
+Evidências precisam ser confiáveis'''
+
+
+
+
+
+
 
 
 
---
+++ operadores_aritimetico,_relacionais_e_logicos.py
@@ -0,0 +1,36 @@
+'''a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)'''
+
+
+'''x = 10 
+y = 5
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= 10)
+print(y <= 5)'''
+
+
+print(True and True)
+print(True and False)
+
+print(True or False)
+print(False or False)
+
+print(not True)
+print(not False)
+
+
+
+
+
+
