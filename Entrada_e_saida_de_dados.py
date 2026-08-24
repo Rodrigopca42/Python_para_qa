d868cf7cad3a25595ced86425999eb4aa11cea5c
Rodrigopca42 <rodrigopca426281@gmail.com>
2026-08-24 18:30:32 -0300

    Entrada e saída de dados

---
+++ Entrada_e_saida_de_dados.py
@@ -0,0 +1,24 @@
+'''nome = input('Digite seu nome: ')
+
+print('Olá ',nome)'''
+
+'''idade = input("Digite sua idade: ")
+print(idade)
+print(type(idade))'''
+
+'''nome = input("Digite seu nome: ")
+print("Bem-vindo,", nome)'''
+
+cidade = input("Digite sua cidade: ")
+print(f"Cidade informada: {cidade}")
+
+print(type(cidade))
+
+
+
+
+
+
+
+
+
